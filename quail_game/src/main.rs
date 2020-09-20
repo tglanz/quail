33bcf4c2e22f64d855c374502caa442c333b4cd8
Tal Glanzman <talglanzman@gmail.com>
2020-09-20 20:14:48 +0300

    escape on exit

--- quail_game/src/main.rs
+++ quail_game/src/main.rs
@@ -42,6 +42,7 @@ impl Plugin for GamePlugin{
                 ..Default::default()
             })
             .add_startup_system(setup.system())
+            .add_system(bevy::input::system::exit_on_esc_system.system())
             ;
     }   
 }
